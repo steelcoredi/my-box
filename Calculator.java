4dac5c777c5fb3ab6110c33d7c476417604136d7
Dmitry Petrov <steel.core@inbox.ru>
2024-06-18 08:19:36 +0300

    Main method refactored

--- Calculator.java
+++ Calculator.java
@@ -29,15 +29,15 @@ public class Calculator {
 
   private static boolean isRoman(String ex) {
     return ex.matches("[IVX+\\-*/]+");
-  }    // возвращиет в 15стр, мачес это совпадает
+  }
 
   private static int romanToInteger(String roman) {
     int result = 0; //
     int prevValue = 0;
-    for (char c : roman.toCharArray()) { //
-	int value = toArabMap.get(c); // метод мап ключем значения с
+    for (char c : roman.toCharArray()) {
+	int value = toArabMap.get(c);
 	if (value > prevValue) {
-	  result += value - 2 * prevValue; // к чатботу
+	  result += value - 2 * prevValue;
 	  result += value;
 	}
 	prevValue = value;
@@ -46,7 +46,7 @@ public class Calculator {
   }
 
   private static String integerToRoman(int num) {
-    StringBuilder sb = new StringBuilder(); // созд массив из симв и прилепляется символы
+    StringBuilder sb = new StringBuilder();
     int[] arr = {10, 9, 5, 4, 1};
     String[] symbols = {"X", "IX", "V", "IV", "I"};
     for (int i = 0; i < arr.length && num >= 0; i++) {
@@ -90,21 +90,21 @@ public class Calculator {
     return result;
   }
 
-  private static String calcRom(String expression) throws Exception { // выбрасывает исключение
-    String[] signs = expression.split("(?<=[-+*/])|(?=[-+*/])"); // разделяем
+  private static String calcRom(String expression) throws Exception {
+    String[] signs = expression.split("(?<=[-+*/])|(?=[-+*/])");
     int c = romanToInteger(signs[0]); // к 33
     if (c < 1 || c > 10) {
 	throw new Exception("Числа должны быть от I до X включительно"); //
     }
-    for (int i = 1; i < signs.length; i += 2) { // что то связаное с римкими цифрами, их расположение и тп заморочки , +=чтобы перепрыгивал через оператор
+    for (int i = 1; i < signs.length; i += 2) {
 	String operator = signs[i];
 	int operand = romanToInteger(signs[i + 1]);
 	if (operand < 1 || operand > 10) { // если
-	  throw new Exception("Числа должны быть от I до X включительно"); // текст сообщения об ошибке
+	  throw new Exception("Числа должны быть от I до X включительно");
 	}
 	switch (operator) {
 	  case "+":
-	    c += operand; // если то то это
+	    c += operand;
 	    break;
 	  case "-":
 	    c -= operand;
@@ -116,7 +116,7 @@ public class Calculator {
 	    c /= operand;
 	    break;
 	  default:
-	    throw new Exception("Не допустимый оператор"); // снова текст
+	    throw new Exception("Не допустимый оператор");
 	}
     }
     return integerToRoman(c);
